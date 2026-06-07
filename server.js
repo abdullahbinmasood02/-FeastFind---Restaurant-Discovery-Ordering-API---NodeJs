5dd75b0e6487dd6a770d70d3b3bcae0bf28127ad
abdullah <abdullahbinmasood02@gmail.com>
2026-06-07 12:43:40 +0500

    adding initial files

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const app = require("./app");
+
+//handling exceptions and unhandled promises
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message, err.stack);
+  process.exit(1);
+});
+
+//injecting env
+dotenv.config({
+  path: "./config.env",
+});
+
+//connecting to mongoDB
+const DB = process.env.DB.replace("<PASSWORD>", process.env.PASSWORD);
+mongoose.connect(DB).then(() => console.log("Database Connected"));
+
+const PORT = process.env.PORT || 3000;
+
+//connecting to server
+const server = app.listen(PORT, () =>
+  console.log(`server listening at port ${PORT}`),
+);
