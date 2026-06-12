176e7ec6d01d9efdc72fa60aac52aefb26f29fef
abdullah <abdullahbinmasood02@gmail.com>
2026-06-12 20:33:19 +0400

    added cookie based responses for sending token

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,10 +15,19 @@ function generateJwt(id, role) {
 
 function sendResponse(id, role, statusCode, res) {
   const token = generateJwt(id, role);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === "prod") cookieOptions.secure = true;
+
+  res.cookie("jwt", token, cookieOptions);
 
   res.status(statusCode).json({
     status: "success",
-    token,
   });
 }
 
